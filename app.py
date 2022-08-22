415134ce2706dc78a3d0c17129577502094e1d19
Loukya Munugala <loukyamunugala@raghu-mac.local>
2022-08-22 16:59:28 -0700

    Deliverable data.

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask_pymongo import PyMongo
 import scraping
 app = Flask(__name__)
 # use flask_pymongo to set up mongo connection
-app.config["MONGO_URL"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_templete("index.html", mars=mars)
+    return render_template("index.html", mars=mars)
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
     mars_data = scraping.scrape_all()
-    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    mars.insert_one(mars_data)
     return redirect('/', code=302)
 if __name__ == "__main__":
     app.run()
--- scraping.py
+++ scraping.py
@@ -1,65 +1,135 @@
 import pandas as pd
+import datetime as dt
 
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Set up Splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # Set up Splinter
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
 
+    news_title, news_paragraph = mars_news(browser)
 
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
+    # Run all scraping functions and store in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
+    }
+    
+    browser.quit()
+    return data
+    
+def mars_news(browser):
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Visit the mars nasa news site
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    browser.visit(url)
 
-html = browser.html
-news_soup = soup(html, 'html.parser')
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+    
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-slide_elem = news_soup.select_one('div.list_text')
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
 
-# Use the parent element to find the first `a` tag and save it as `news_title`
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+        # Use the parent element to find the first `a` tag and save it as `news_title`
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+    
 
-# Use the parent element to find the paragraph text
-new_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-new_p
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
 
 
 # ### Featured Images
-# visit url
-url = 'https://spaceimages-mars.com/'
-browser.visit(url)
+def featured_image(browser):
+    # visit url
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_= "fancybox-image").get('src') 
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_= "headerimage fade-in").get('src') 
-img_url_rel
+    except AttributeError:
+        return None
+    
+    # Use the base URL to create an absolute URL
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
 
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    return img_url
 
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
+# Mars Facts
+def mars_facts():
+    try:
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+    
+    except BaseException:
+        return None
 
-df.to_html()
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
-browser.quit()
+    return df.to_html(classes="table table-striped")
 
+# Challenge
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+# 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+# 3. Write code to retrieve the image urls and titles for each hemisphere.
+    links = browser.find_by_css('a.product-item h3')
+    for index in range(len(links)):
+        
+        browser.find_by_css('a.product-item h3')[index].click()
+        hemisphere_data = scrape_hemisphere(browser.html)
+        hemisphere_image_urls.append(hemisphere_data)
+    
+        browser.back()
+
+# 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
+
+def scrape_hemisphere(html_text):
+    hemi_soup = soup(html_text, "html.parser")
+    try:
+        title_element = hemi_soup.find("h2", class_="title").get_text()
+        sample_element = hemi_soup.find("a", text="Sample").get("href")
+    except AttributeError:
+        title_element = None
+        sample_element = None
+    hemisphere_dictionary = {
+        "title": title_element,
+        "img_url": sample_element
+    }
+    return hemisphere_dictionary
+
+if __name__=="__main__":
+    print(scrape_all())
+    
 
 
